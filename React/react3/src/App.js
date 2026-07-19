49f897424954dbbc11f6e885e6fcf448dde372d2
adithichiripal <155903049+adithichiripal@users.noreply.github.com>
2026-07-19 12:14:41 +0530

    Create App.js

---
+++ React/react3/src/App.js
@@ -0,0 +1,18 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+
+  return (
+    <div>
+      <CalculateScore
+        name="Adithi"
+        school="VIT-AP University"
+        total={480}
+        goal={6}
+      />
+    </div>
+  );
+
+}
+
+export default App;

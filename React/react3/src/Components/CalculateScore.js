b9055972b1c834e34c90022332db215567b9389f
adithichiripal <155903049+adithichiripal@users.noreply.github.com>
2026-07-19 12:14:16 +0530

    Create CalculateScore.js

---
+++ React/react3/src/Components/CalculateScore.js
@@ -0,0 +1,24 @@
+import "../Stylesheets/mystyle.css";
+
+function CalculateScore(props) {
+
+    const average = props.total / props.goal;
+
+    return (
+        <div className="container">
+            <h2>Student Score Calculator</h2>
+
+            <p><b>Name :</b> {props.name}</p>
+
+            <p><b>School :</b> {props.school}</p>
+
+            <p><b>Total Score :</b> {props.total}</p>
+
+            <p><b>Number of Subjects :</b> {props.goal}</p>
+
+            <h3>Average Score : {average}</h3>
+        </div>
+    );
+}
+
+export default CalculateScore;
